b53f192aa98e4938dd38f637acba31ae3f39e9df
Sam <stmiyamoto10@gmail.com>
2022-04-25 07:30:33 -0700

    Added practice.js

---
+++ practice.js
@@ -0,0 +1,14 @@
+//Traditional function
+function regFunc (a)
+{
+    return a + 10;
+}
+
+console.log(regFunc(10))
+
+//Arrow function
+a = 10;
+
+const arrowFunc = () => a + 10;
+
+console.log(arrowFunc)
