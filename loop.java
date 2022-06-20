eaf726fd60d125a0bcf8b00e5a95d4992574bc6d
Sam <stmiyamoto10@gmail.com>
2022-06-20 11:11:27 -0700

    First loop in Java

---
+++ loop.java
@@ -0,0 +1,14 @@
+// Looping in Java
+// Printing numbers 0-4
+
+class MyClass {
+    //contents of the class (fields and methods)
+    public static void main(String[] args){
+
+        // code to be executed
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        };
+      }
+
+ }
